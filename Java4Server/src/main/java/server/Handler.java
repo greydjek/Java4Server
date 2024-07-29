05d178a006d300a95b9ecd5df234046994e43037
Artem <delux26rus@mail.ru>
2024-07-29 23:18:55 +0300

    32 min

--- Java4Server/src/main/java/server/Handler.java
+++ Java4Server/src/main/java/server/Handler.java
@@ -4,12 +4,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Handler implements Runnable {
-
+    private Path root;
+    private Path userDir;
     private String userName;
     private int counter = 0;
     private Socket socket;
@@ -19,8 +24,16 @@ public class Handler implements Runnable {
     private SimpleDateFormat format;
 
     public Handler(Socket socket, Server server) throws IOException {
+        root = Paths.get("C:\\Education\\Java4\\Java4Server\\Java4Server\\Server");
+        if (!Files.exists(root)) {
+            Files.createDirectory(root);
+        }
         counter++;
         userName = "User#" + counter;
+        userDir = root.resolve(userName);
+        if (!Files.exists(userDir)) {
+            Files.createDirectory(userDir);
+        }
         this.socket = socket;
         this.server = server;
         dis = new DataInputStream(socket.getInputStream());
@@ -33,8 +46,11 @@ public class Handler implements Runnable {
     public void run() {
         try {
             while (true) {
-                String message = dis.readUTF();
-
+                String fileName = dis.readUTF();
+                long size = dis.readLong();
+   Path path = userDir.resolve(fileName);
+    Files.copy(dis, path, StandardCopyOption.REPLACE_EXISTING);
+    responseOk();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,6 +65,12 @@ public class Handler implements Runnable {
 
     public String getMessage(String message) {
         return getTime() + "[" + userName + "]" + message;
+    }
+
+    private void responseOk() throws IOException {
+        dos.writeUTF(getMessage("Response OK "));
+        dos.flush();
+
     }
 
     public void sendMessage(String message) throws IOException {
